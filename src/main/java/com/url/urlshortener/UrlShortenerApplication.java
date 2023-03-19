9a331100244a57de1222e8af9ba1e4b2d72d5f45
YOUNJEI <kyj951211@gmail.com>
2023-03-19 12:12:08 +0900

    feat: keycloak security config

--- src/main/java/com/url/urlshortener/UrlShortenerApplication.java
+++ src/main/java/com/url/urlshortener/UrlShortenerApplication.java
@@ -1,7 +1,10 @@
 package com.url.urlshortener;
 
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UrlShortenerApplication {
@@ -10,4 +13,8 @@ public class UrlShortenerApplication {
 		SpringApplication.run(UrlShortenerApplication.class, args);
 	}
 
+	@Bean
+	public KeycloakConfigResolver keycloakConfigResolver() {
+		return new KeycloakSpringBootConfigResolver();
+	}
 }
